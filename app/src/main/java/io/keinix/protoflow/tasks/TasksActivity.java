55d1cc9f4ba53a2632fbd28cc8a16a03c53fff4c
Keinix <zackcosborn@gmail.com>
2018-07-13 15:43:18 +0900

    added project section to addEditTask menu in UI

--- app/src/main/java/io/keinix/protoflow/tasks/TasksActivity.java
+++ app/src/main/java/io/keinix/protoflow/tasks/TasksActivity.java
@@ -6,6 +6,7 @@ import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -123,7 +124,7 @@ public class TasksActivity extends DaggerAppCompatActivity
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
         switch (id) {
