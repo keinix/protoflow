25b1948337fc50e73713839119b63eec5f913b29
Keinix <zackcosborn@gmail.com>
2018-09-30 16:24:14 +0900

    finished code for basic repeating notifications

--- app/src/main/java/io/keinix/protoflow/scheduling/RepeatingNotificationWorker.java
+++ app/src/main/java/io/keinix/protoflow/scheduling/RepeatingNotificationWorker.java
@@ -2,16 +2,27 @@ package io.keinix.protoflow.scheduling;
 
 import android.support.annotation.NonNull;
 
+import androidx.work.Data;
 import androidx.work.Worker;
 
 public class RepeatingNotificationWorker extends Worker {
 
+    private int taskId;
+    private String taskName;
+    private long taskStartTime;
+
     @NonNull
     @Override
     public Result doWork() {
-        NotificationScheduler.schedulePeriodicNotification();
+        getData();
+        NotificationScheduler.schedulePeriodicNotification(taskId, taskName, taskStartTime);
         return Result.SUCCESS;
     }
 
-
+    private void getData() {
+        Data data = getInputData();
+        taskId = data.getInt(NotificationScheduler.INPUT_ID, 0);
+        taskName = data.getString(NotificationScheduler.INPUT_TITLE);
+        taskStartTime = data.getLong(NotificationScheduler.INPUT_START_TIME, 0);
+    }
 }
